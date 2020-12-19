58dd8aa953c553546a2476131bf751b523728d17
@KH45 <kat.herasymenko@gmail.com>
2020-12-19 01:11:30 +0100

    Д/З урок 2 Катерина

---
+++ starter/lesson 2/Lesson 2 - Kateryna/Задание 1.py
@@ -0,0 +1,6 @@
+word1 = input("Please enter the first word ")
+word2 = input("Please enter the second word ")
+
+result = word1 + "," + word2
+
+print(result)
---
+++ starter/lesson 2/Lesson 2 - Kateryna/Задание 2.py
@@ -0,0 +1,7 @@
+a = input("Please enter the first number ")
+b = input("Please enter the second number ")
+x = input("Please enter the third number ")
+
+result = a < x < b
+
+print(result)
