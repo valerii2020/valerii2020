36745f3cbb9f330d545aa0cd26457d6418c0a416
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-20 18:04:52 +0000

    Comma task

---
+++ starter/lesson 2/ViolettaP/homework2.py
@@ -0,0 +1,6 @@
+#task one, ask for two word and display them with a comma
+
+word1 = input("Please enter the first word:  ")
+word2 = input("Please enter the second word here: ")
+
+print(word1,  word2, sep=",")
