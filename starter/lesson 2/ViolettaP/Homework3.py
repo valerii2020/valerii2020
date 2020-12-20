d074a3e19c2d8e31d3d43d35ac21ca95a906c921
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-20 18:10:15 +0000

    Is the X between a and b?

---
+++ starter/lesson 2/ViolettaP/Homework3.py
@@ -0,0 +1,10 @@
+#ask for int
+
+x = int(input("Please enter X number: "))
+a = int(input("Please enter A number: "))
+b = int(input("Please enter B number: "))
+
+if a<x<b:
+    print(True)
+else:
+    print(False)
