f8097eedf719b4e46897e77ae4463132ee4ac0d0
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-20 22:32:00 +0000

    I dont know about cos and tan??/ math.tan or tan() ??

--- starter/lesson 2/ViolettaP/homework4.py
+++ starter/lesson 2/ViolettaP/homework4.py
@@ -14,6 +14,6 @@ elif d == 0:
     print ("This equation has one solutions: ", x)
 else:
     x1 = (-b+math.sqrt(b**2-4*a*c))/2*a
-    x2 = (-b-math.sqrt(b**2-4*a*c))/2*a
-    print ("This equation has two solutions: ", x1, " and", x2)
+    x2 = (-b-math.sqrt(b**2-4*a*c))/2*a, " and", x2)
 
+    print ("This equation has two solutions: ", x1
