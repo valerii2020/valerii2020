51435bd9d8d3e77b309c4d748ac82ce5f17a2d64
Violetta Pavlova <msviopavlova@gmail.com>
2020-12-21 09:49:41 +0000

    You are not Violetta

---
+++ starter/lesson 3/ViolettaP/lesson3homework2.py
@@ -0,0 +1,6 @@
+name1 = "Violetta"
+name = input("Please enter your name: ")
+if name == name1:
+    print("Hello, ", name1)
+else:
+    print("You are ", name, ", not" ,name1, ",so you have no access")
